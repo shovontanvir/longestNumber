8e91528a793cb8762e4406dae14e7adcd756f432
shovon tanvir <shovontanvir@shovons-MacBook-Pro.local>
2022-05-30 16:54:02 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const points = [1, 40, 100, 5, 25, 10, 108];
+
+
+document.querySelector("#array").innerHTML = "The array is " + points;
+
+function numSort(arrayName) {
+  for (var i=0; i <= arrayName.length; i++)   {
+    const arrayTwo = arrayName.map(myFunc);
+  }
+
+  function myFunc(values, index, arrray){
+    if(arrayName[index] < arrayName[index + 1]){
+      let temp = arrayName[index];
+      arrayName[index] = arrayName[index + 1];
+      arrayName[index + 1] = temp;
+    }
+  }
+  return arrayName;
+}
+
+let sorted = numSort(points);
+
+document.querySelector("#sorted").innerHTML = "The sorted array is " + sorted;
+
+document.querySelector("#longest").innerHTML = "The highest number in the array is " + sorted[0];
