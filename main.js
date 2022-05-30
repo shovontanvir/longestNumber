192f86ccf611df28d7d0fef0a213156d68eff7d3
shovon tanvir <shovontanvir@shovons-MacBook-Pro.local>
2022-05-30 17:47:43 +0600

    fourth commit

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-const points = [1, 40, 100, 5, 25, 10, 108];
+function sort(){
+    let arrayInput = document.querySelector("#array").value;
 
+    console.log(arrayInput);
+    let points = arrayInput.split(",");
+    document.querySelector("#mainArray").innerHTML = "The original array is " + points;
 
-document.querySelector("#array").innerHTML = points;
+    function numSort(arrayName) {
+        for (var i=0; i <= arrayName.length; i++)   {
+            const arrayTwo = arrayName.map(myFunc);
+        }
 
-function numSort(arrayName) {
-  for (var i=0; i <= arrayName.length; i++)   {
-    const arrayTwo = arrayName.map(myFunc);
-  }
-
-  function myFunc(values, index, arrray){
-    if(arrayName[index] < arrayName[index + 1]){
-      let temp = arrayName[index];
-      arrayName[index] = arrayName[index + 1];
-      arrayName[index + 1] = temp;
+        function myFunc(values, index, arrray){
+            if(Number(arrayName[index]) < Number(arrayName[index + 1])){
+                let temp = arrayName[index];
+                console.log(typeof(temp));
+                arrayName[index] = arrayName[index + 1];
+                arrayName[index + 1] = temp;
+            }
+        }
+        return arrayName;
     }
-  }
-  return arrayName;
-}
 
-let sorted = numSort(points);
-
-
-function clickSort(){
     
+
+
+    let sorted = numSort(points);
+
     document.querySelector("#sorted").innerHTML = "The sorted array is " + sorted;
 
     document.querySelector("#longest").innerHTML = "The highest number in the array is " + sorted[0];
+
 }
